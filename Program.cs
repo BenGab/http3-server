3f3eca80a2045a797d034dbca00fe85ecfdc8e61
bengab <benkogabor@msn.com>
2023-11-23 11:21:36 +0100

    http3 server init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((context, options) => {
+    options.ListenLocalhost(5001, listenOptions => {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
