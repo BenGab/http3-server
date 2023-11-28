3379db798320fd7de162ae8f57e5ab184f6e6872
bengab <benkogabor@msn.com>
2023-11-28 19:41:13 +0100

    using in memey threadsafe cache

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,28 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 public class HomeController : ControllerBase {
-    const string seriesMainpath = "/home/bengab/Development/series"; 
+    const string seriesMainpath = "/home/bengab/Development/series";
+    private readonly ConcurrentDictionary<string, byte[]> dicomCache;
+
+    private void LoadCacheBySeries(string[] filepaths) {
+        foreach(var path in filepaths) {
+            var filename = Path.GetFileNameWithoutExtension(path)?.Replace("image-", string.Empty);
+
+            if(filename == null) {
+                continue;
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(path);
+            dicomCache.TryAdd(filename, bytes);
+        }
+    }
+
+    public HomeController()
+    {
+        dicomCache = new ConcurrentDictionary<string, byte[]>();
+    }
 
     [Route("")]
     [Route("Home")]
@@ -28,13 +48,24 @@ public class HomeController : ControllerBase {
             return [];
         }
 
-        return Directory.GetFiles(path).AsEnumerable()
+        var files = Directory.GetFiles(path);
+        Task.Factory.StartNew(() => {
+            LoadCacheBySeries(files);
+        }, TaskCreationOptions.DenyChildAttach);
+
+        return files.AsEnumerable()
                 .Select(path => Path.GetFileNameWithoutExtension(path) ?? string.Empty)
                 .Where(x => !string.IsNullOrEmpty(x)).Select(x => x.Replace("image-", string.Empty)).ToList();
     }
 
     [Route("/studies/{study}/series/{series}/instances/{instance}")]
     public FileStreamResult GetInstance(string study, string series, string instance) {
+        byte[] fileStreamBytes;
+        if(dicomCache.TryGetValue(instance, out fileStreamBytes)) {
+            Stream result = new MemoryStream(fileStreamBytes);
+            return new FileStreamResult(result, "application/dicom");
+        }
+
         string path = $"{seriesMainpath}/series-{series}/image-{instance}.dcm";
         Stream stream = System.IO.File.Open(path, FileMode.Open, FileAccess.Read);
  
